6e6b011cd852cff6ce1e18781089861e4c98af8b
Julian Peeters <julianpeeters@gmail.com>
2018-01-27 17:50:43 -0800

    Restore pureconfig

--- modules/server/jvm/src/main/scala/com/julianpeeters/example/server/Server.scala
+++ modules/server/jvm/src/main/scala/com/julianpeeters/example/server/Server.scala
@@ -1,19 +1,19 @@
 package com.julianpeeters.albertbernardcheryl.server
 
-import scala.util.Properties.envOrNone
+import com.julianpeeters.example.server.config.Config
+
 import cats.effect._
+import eu.timepit.refined.auto._
 import fs2._
 import org.http4s.server.blaze.BlazeBuilder
 
 object Server extends StreamApp[IO] {
 
-  val ip: String = "0.0.0.0"
-
   override def stream(args: List[String], requestShutdown: IO[Unit]): Stream[IO, StreamApp.ExitCode] =
     for {
-      port <- Stream.eval(IO(envOrNone("PORT").map(_.toInt).getOrElse(8080)))
+      cfg      <- Config.load[IO]
       exitCode <- BlazeBuilder[IO]
-        .bindHttp(port, ip)
+        .bindHttp(cfg.http.port, cfg.http.host)
         .mountService(Service.endpoints)
         .mountService(Service.assets, s"/${BuildInfo.assetsRoot}")
         .serve
